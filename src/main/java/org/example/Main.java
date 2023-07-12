8c833a01a49f08d54b1f953ff19fa03566f3f327
Frederik Hahne <atomfrede@gmail.com>
2023-07-12 20:04:29 +0200

    :hankey: sample project via gradle init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,6 @@ import org.openrewrite.config.Environment;
 import org.openrewrite.gradle.AddDependency;
 import org.openrewrite.gradle.GradleParser;
 import org.openrewrite.internal.InMemoryLargeSourceSet;
-import org.openrewrite.java.tree.J;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,27 +12,26 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        Path projectDir = Paths.get("./test-gradle");
-        Environment environment = Environment.builder().scanRuntimeClasspath().build();
+        Path projectDir = Paths.get("./test-gradle-2");
         AddDependency recipe = new AddDependency(
                 "org.openrewrite",
                 "rewrite-java",
                 "8.1.2",
                 null,
                 "implementation",
-                "",
+                "java.util.String",
                 null,
                 null,
                 null,
                 true
         );
-        List<Path> sourcePaths = Files.find(projectDir, 999, (p, bfa) ->
-                        bfa.isRegularFile())
+        List<Path> sourcePaths = Files.find(projectDir, 999, (p, bfa) -> {
+                        return bfa.isRegularFile()  && p.getFileName().toString().endsWith(".gradle");}
+                )
                 .toList();
 
         GradleParser gradleParser = GradleParser.builder().build();
@@ -45,7 +43,7 @@ public class Main {
         InMemoryLargeSourceSet inMemoryLargeSourceSet = new InMemoryLargeSourceSet(cus);
 
 
-        RecipeRun run = recipe.run(inMemoryLargeSourceSet, ctx);
+        RecipeRun run = recipe.run(inMemoryLargeSourceSet, ctx, 25);
         Changeset changeset = run.getChangeset();
         List<Result> allResults = changeset.getAllResults();
 
---
+++ test-gradle/src/main/java/atomfrede/com/github/sample/Sample.java
@@ -0,0 +1,7 @@
+package atomfrede.com.github.sample;
+public class Sample {
+    public static void main(String[] args) throws IOException {
+
+        String s = "Test";
+    }
+}
