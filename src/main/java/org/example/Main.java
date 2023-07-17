5a33b1c22520a6476b8667fa9b32133afafdfb70
Frederik Hahne <atomfrede@gmail.com>
2023-07-17 17:56:25 +0200

    :hankey: update sample projects

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,57 +1,31 @@
 package org.example;
 
-import org.openrewrite.*;
-import org.openrewrite.config.Environment;
-import org.openrewrite.config.ResourceLoader;
-import org.openrewrite.gradle.AddDependency;
-import org.openrewrite.gradle.GradleParser;
-import org.openrewrite.internal.InMemoryLargeSourceSet;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static java.util.Collections.emptyList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static final ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         Path projectDir = Paths.get("./test-gradle");
-        AddDependency recipe = new AddDependency(
-                "com.google.guava",
-                "guava",
-                "29.X",
-                "-jre",
-                "implementation",
-                "org.junit.jupiter.api.*",
-                "test",
-                "jar",
-                "com.fasterxml.jackson*",
-                true
-        );
-        List<Path> sourcePaths = Files.find(projectDir, 999, (p, bfa) -> {
-                        return bfa.isRegularFile()  && (p.getFileName().toString().endsWith(".gradle") || p.getFileName().toString().endsWith(".java"));}
-                )
-                .toList();
 
-        GradleParser gradleParser = GradleParser.builder().build();
-        ExecutionContext ctx = new InMemoryExecutionContext(Throwable::printStackTrace);
+        File file = projectDir.toFile();
+        ProcessBuilder builder = new ProcessBuilder("gradle", "--init-script", "init.gradle", "rewriteDryRun").directory(file);
+        Process process = builder.start();
+        StreamGobbler streamGobbler =
+                new StreamGobbler(process.getInputStream(), System.out::println);
+        Future<?> future = executorService.submit(streamGobbler);
 
-        List<SourceFile> cus = gradleParser.parse(sourcePaths, projectDir, ctx)
-                .collect(Collectors.toList());
+        int exitCode = process.waitFor();
 
-        InMemoryLargeSourceSet inMemoryLargeSourceSet = new InMemoryLargeSourceSet(cus);
+        System.exit(exitCode);
 
-
-        RecipeRun run = recipe.run(inMemoryLargeSourceSet, ctx, 25);
-        Changeset changeset = run.getChangeset();
-        List<Result> allResults = changeset.getAllResults();
-
-        for (Result allResult : allResults) {
-            System.out.println(allResult.diff(projectDir));
-        }
     }
 }
